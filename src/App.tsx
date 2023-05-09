6a2635138e69d7472f58b39176fd88e081490b76
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-05-09 11:47:24 -0300

    datadogLogs.init

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <button onClick={() => {
+          throw new Error(String(new Date()))
+        }}>
+          produce error
+        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { datadogLogs } from '@datadog/browser-logs'
+
+datadogLogs.init({
+  clientToken: import.meta.env.VITE_DATADOG_CLIENT_TOKEN,
+  site: 'us5.datadoghq.com',
+  env: import.meta.env.DEV ? 'local' : 'production',
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).datadogLogs = datadogLogs
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
